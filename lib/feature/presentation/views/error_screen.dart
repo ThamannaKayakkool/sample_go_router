0342bc717ef65a3bab52e6b28e8c2ca33313832f
Thamanna <thamannausman0@gmail.com>
2025-12-04 19:59:15 +0530

    initial commit

--- lib/feature/presentation/views/error_screen.dart
+++ lib/feature/presentation/views/error_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:sample_go_router/core/utils/router.dart';
+
 
 class ErrorScreen extends StatelessWidget {
   const ErrorScreen({super.key});
